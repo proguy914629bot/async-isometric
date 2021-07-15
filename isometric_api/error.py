0ae241881d789e980efb286f549eba7c544630f0
proguy914629 <74696067+proguy914629bot@users.noreply.github.com>
2021-07-15 19:20:19 +0700

    Update error.py

--- isometric_api/error.py
+++ isometric_api/error.py
@@ -1,2 +1,17 @@
 class IsometricError(Exception):
-    """The base exception for the Isometric API."""
+    """The base exception for the Isometric API.
+    
+    Attributes
+    ----------
+    resp: :class:`aiohttp.ClientResponse`
+        The response used to make the call to the API.
+    error :class:`str`
+        The error sent by the API.
+    status :class:`int`
+        The status code of the response.
+    """
+    def __init__(self, resp, error):
+        self.resp = resp
+        self.error = error
+        self.status = resp.status
+        super().__init__("Status code %s: %s" % (str(self.status), str(self.error)))
