498e53b4d7b397e93f7fec535dd17b43eb76704c
proguy914629 <74696067+proguy914629bot@users.noreply.github.com>
2021-07-15 20:44:32 +0700

    Update save.py

--- examples/save.py
+++ examples/save.py
@@ -1,5 +1,6 @@
-from isometric_api import Client
+import asyncio # For running corountines
+from isometric_api import Client # Import the Client class
 
-cli = Client()
+cli = Client() # Initiate the class
 
-asyncio.run(cli.isometric("11111 11111 11411 11411 11411 11411 31441 33144 23114 23314 22314- 0 0555 0505 0535- 0 0565 0606 0535- 0 0555 0555 0555", save_to='/path/to/file.png'))
+asyncio.run(cli.isometric("11111 11111 11411 11411 11411 11411 31441 33144 23114 23314 22314- 0 0555 0505 0535- 0 0565 0606 0535- 0 0555 0555 0555", save_to='/path/to/file.png')) # Run the isometric.
