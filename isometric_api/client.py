29d1c54f33675eefec833b08621f1fffadda3e30
proguy914629 <74696067+proguy914629bot@users.noreply.github.com>
2021-07-15 18:12:48 +0700

    Update client.py

--- isometric_api/client.py
+++ isometric_api/client.py
@@ -1,2 +1,13 @@
+import aiohttp, yarl
+
 class Client:
-    pass
+    def __init__(self, *, session: aiohttp.ClientSession = None, loop = None):
+        self._base_url = yarl.URL("https://jeyy-api.herokuapp.com/")
+    
+    @property
+    def url(self) -> str:
+        return str(self._base_url)
+    
+    @url.setter
+    def set_url(self, value):
+        self._base_url = yarl.URL(str(value))
