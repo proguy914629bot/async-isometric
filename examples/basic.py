a46861aa2dc64d108d12f615bc424bf554c43f71
proguy914629 <74696067+proguy914629bot@users.noreply.github.com>
2021-07-15 20:45:36 +0700

    Update basic.py

--- examples/basic.py
+++ examples/basic.py
@@ -1,12 +1,12 @@
-import asyncio
-from isometric_api import Client
+import asyncio # For running corountines
+from isometric_api import Client # Import the Client class
 
-cli = Client()
+cli = Client() # Initiate the Client
 
-async def run():
-    iso = await cli.isometric("11111 11111 11411 11411 11411 11411 31441 33144 23114 23314 22314- 0 0555 0505 0535- 0 0565 0606 0535- 0 0555 0555 0555")
+async def run(): # Make the corountine
+    iso = await cli.isometric("11111 11111 11411 11411 11411 11411 31441 33144 23114 23314 22314- 0 0555 0505 0535- 0 0565 0606 0535- 0 0555 0555 0555") # Run the isometric
 
     iso.fp # --> `io.BytesIO` object.
     iso.resp # --> `aiohttp.ClientResponse` object.
 
-asyncio.run(run())
+asyncio.run(run()) # Run the corountine function we made above.
