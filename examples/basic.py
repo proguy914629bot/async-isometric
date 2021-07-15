33622b1e503da0fe4624b8c1e9cabc46b43d0ab3
proguy914629 <74696067+proguy914629bot@users.noreply.github.com>
2021-07-15 20:41:19 +0700

    Update basic.py

--- examples/basic.py
+++ examples/basic.py
@@ -1,1 +1,8 @@
+from isometric_api import Client
 
+cli = Client()
+
+iso = await cli.isometric("11111 11111 11411 11411 11411 11411 31441 33144 23114 23314 22314- 0 0555 0505 0535- 0 0565 0606 0535- 0 0555 0555 0555")
+
+iso.fp # --> `io.BytesIO` object.
+iso.resp # --> `aiohttp.ClientResponse` object.
