430c1bb6a6e0cabe6f9cc46e26f9cb32e24271c3
proguy914629 <74696067+proguy914629bot@users.noreply.github.com>
2021-08-01 17:38:43 +0700

    Update client.py

--- isometric_api/client.py
+++ isometric_api/client.py
@@ -2,17 +2,7 @@ import aiohttp, yarl, asyncio, pathlib, typing
 from io import BytesIO
 from .errors import *
 from .dataclass import *
-
-async def raise_error(resp):
-    try:
-        js = await resp.json()
-    except:
-        try:
-            js = {'info': (await resp.text())}
-        except:
-            return
-    
-    raise IsometricError(resp, js.get('info', js))
+from .isometric import *
 
 class Client:
     """
@@ -49,7 +39,7 @@ class Client:
     __slots__ = ('_base_url', 'session', 'loop', 'url')
     
     def __init__(self, *, session: typing.Optional[aiohttp.ClientSession] = None, loop = None):
-        self._base_url = yarl.URL("https://jeyy-api.herokuapp.com/")
+        self._base_url = yarl.URL(self._generate_url("https://jeyy-api.herokuapp.com/"))
         self.session = session or aiohttp.ClientSession()
         self.loop = loop or asyncio.get_event_loop()
     
@@ -60,39 +50,27 @@ class Client:
     
     @url.setter
     def set_url(self, value):
-        self._base_url = yarl.URL(str(value))
-
-    async def isometric(self, iso_code: str, *, save_to: str = None) -> IsometricData:
-        """Runs an Isometric.
+        self._base_url = yarl.URL(self._generate_url(value))
         
-        Parameters
-        ----------
-        iso_code: :class:`str`
-            The Isometric Code.
-        save_to: :class:`str`
-            Where to save the isometric file.
-            Defaults to None (Doesn't save)
+    async def _raise_error(resp):
+        try:
+            js = await resp.json()
+        except:
+            try:
+                js = {'info': (await resp.text())}
+            except:
+                return
+    
+        raise IsometricError(resp, js.get('info', js))
+        
+    def _generate_url(self, url):
+        uri = str(url)
+        
+        if uri.endswith("/"):
+            uri = uri[:-1]
             
-        Raises
-        ------
-        IsometricError
-            This raises when a unknown error was found.
-            
-        Returns
-        -------
-        :class:`IsometricData`
-            The Isometric data provided by the API.
-        """
-        uri = str(self._base_url)
-        async with self.session.get(uri, params={"iso_code": str(iso_code)}) as resp:
-            if not resp.content_type.startswith("image/"):
-                await raise_error(resp)
-            
-            fp = BytesIO(await resp.read())
-            if save_to:
-                path = pathlib.Path(str(save_to))
-                path.write_bytes(fp.getbuffer())
-            else:
-                path = None
-                
-            return IsometricData(resp, fp, path)
+        return uri
+
+    @property
+    def isometric(self) -> Isometric:
+        return Isometric(self)
